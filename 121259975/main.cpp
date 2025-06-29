8540a67738254fdd5c2d41dac306a1f19cb4af55
wery0 <wery0123@yandex.ru>
2025-06-29 17:15:11 +0300

    Rm template

--- 121259975/main.cpp
+++ 121259975/main.cpp
@@ -1,39 +1,8 @@
 #pragma GCC optimize("Ofast")
-// #pragma GCC target("avx,avx2,fma")
-
-#include "bits/stdc++.h"
-
-//#define NDEBUG
-#define F first
-#define S second
-#define vec vector
-#define pb push_back
-#define pll pair<ll, ll>
-#define all(m) m.begin(), m.end()
-#define rall(m) m.rbegin(), m.rend()
-#define uid uniform_int_distribution
 #define timeStamp() std::chrono::steady_clock::now()
-#define unify(m) sort(all(m)), m.erase(unique(all(m)), m.end());
-#define duration_micro(a) chrono::duration_cast<chrono::microseconds>(a).count()
 #define duration_milli(a) chrono::duration_cast<chrono::milliseconds>(a).count()
-#define fast cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0);
+#include "bits/stdc++.h"
 using namespace std;
-using str = string;
-using ll = long long;
-using ld = long double;
-mt19937 rnd(timeStamp().time_since_epoch().count());
-mt19937_64 rndll(timeStamp().time_since_epoch().count());
-template<typename T, typename U> bool chmin(T& a, const U& b) {return (T)b < a ? a = b, 1 : 0;}
-template<typename T, typename U> bool chmax(T& a, const U& b) {return (T)b > a ? a = b, 1 : 0;}
-struct custom_hash {static uint64_t xs(uint64_t x) {x += 0x9e3779b97f4a7c15; x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9; x = (x ^ (x >> 27)) * 0x94d049bb133111eb; return x ^ (x >> 31);} template<typename T> size_t operator()(T x) const {static const uint64_t C = timeStamp().time_since_epoch().count(); return xs(hash<T> {}(x) + C);}};
-template<typename K> using uset = unordered_set<K, custom_hash>;
-template<typename K, typename V> using umap = unordered_map<K, V, custom_hash>;
-template<typename T1, typename T2> ostream& operator<<(ostream& out, const pair<T1, T2>& x) {return out << x.F << ' ' << x.S;}
-template<typename T1, typename T2> istream& operator>>(istream& in, pair<T1, T2>& x) {return in >> x.F >> x.S;}
-template<typename T, size_t N> istream& operator>>(istream& in, array<T, N>& a) {for (auto &x : a) in >> x; return in;}
-template<typename T, size_t N> ostream& operator<<(ostream& out, const array<T, N>& a) {for (size_t i = 0; i < a.size(); ++i) {out << a[i]; if (i + 1 < a.size()) out << ' ';} return out;}
-template<typename T> istream& operator>>(istream& in, vector<T>& a) {for (auto& x : a) in >> x; return in;}
-template<typename T> ostream& operator<<(ostream& out, const vector<T>& a) {for (size_t i = 0; i < a.size(); ++i) {out << a[i]; if (i + 1 < a.size()) out << ' ';} return out;}
 
 array<int, 5> mask_to_arr(int mask) {
     assert(0 <= mask && mask < 7776);
@@ -146,7 +115,6 @@ string describe_move(array<int, 5> m, int b) {
  Scores for Three/Four of a kind are approximate since idk what exact formula is used for them
 */
 int main() {
-    fast;
     auto st = timeStamp();
     function<void(vector<int>)> gen_arrs = [&](vector<int> m) {
         if (m.size() == 5) {
